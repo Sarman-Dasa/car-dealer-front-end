3dfcf5a4967b9e149e76a58b365df6179fd761de
Sarman-Dasa <sarmand@zignuts.com>
2024-06-10 12:10:53 +0530

    Crud operation for car detail using firebase

---
+++ src/components/cars/Add.js
@@ -0,0 +1,297 @@
+// Importing necessary libraries
+import React, { useState} from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import "../../css/car.css";
+import FileUpload from "../fileUpload";
+import { addDoc, collection } from "firebase/firestore";
+import { db, storage } from "../firebase/Firebase";
+import { useSelector } from "react-redux";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { v4 as uuid } from "uuid";
+import { toast, ToastContainer } from "react-toastify";
+import Emitter from "../../services/emitter";
+
+// CarForm component
+export default function CarForm() {
+  const [carDetail, setCarDetail] = useState({
+    compna_name: "",
+    model: "",
+    mileage: "",
+    color: "",
+    condition: "",
+    number: "",
+    engine_type: "",
+    price: "",
+    register_date: "",
+    status: "",
+    transmission: "",
+    fuel_type: "",
+    image_url: "",
+  });
+
+  // const [isEnableSubmitBtn,setIsEnableSubmitBtn] = useState(true);
+
+  const user = useSelector((state) => state.app.user);
+
+  const handleFormData = (e) => {
+    const { name, value } = e.target;
+    setCarDetail((preview) => ({
+      ...preview,
+      [name]: value,
+    }));
+  };
+
+  // Handle  submission
+  const handleSubmit = async (event) => {
+    if (carDetail && !carDetail.image_url) {
+      toast.warning("please upload image !");
+      return;
+    }
+    setCarDetail((preview) => ({
+      ...preview,
+      owner_id: user.id,
+    }));
+
+    const carCollection = collection(db, "cars");
+    await addDoc(carCollection, carDetail)
+      .then((response) => {
+        console.log("Response", response);
+        clearData();
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+    console.log("cars", carDetail);
+  };
+
+  // Store file into firebase storege
+  const saveFileData = (e) => {
+    const imageRef = ref(storage, `file/${uuid()}`);
+    uploadBytes(imageRef, e[0].image)
+      .then((response) => {
+        console.log("response", response);
+        // Get the download URL
+        getDownloadURL(imageRef)
+          .then((url) => {
+            console.log("Url", url);
+            setCarDetail((preview) => ({
+              ...preview,
+              image_url: url,
+            }));
+            toast.success("File upload Successfully");
+          })
+          .catch((error) => {
+            console.log("Error", error);
+          });
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  };
+
+  //Clear form data
+  function clearData() {
+    setCarDetail({
+      compna_name: "",
+      model: "",
+      mileage: "",
+      color: "",
+      condition: "",
+      number: "",
+      engine_type: "",
+      price: "",
+      register_date: "",
+      status: "",
+      transmission: "",
+      fuel_type: "",
+      image_url: "",
+    });
+
+    // Call EventEmitter to clear file data 
+    Emitter.emit('CLEAR_FILE_DATA');
+  }
+
+  return (
+    <Container className="car-container">
+      <h2 className="my-4">Car Details Form</h2>
+      <Form>
+        <Row className="mb-3">
+          <Col>
+            <Form.Group controlId="comapanyName">
+              <Form.Label>Company name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Company name"
+                name="compna_name"
+                value={carDetail.compna_name}
+                onChange={handleFormData}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="modal">
+              <Form.Label>Model</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter car model"
+                name="model"
+                value={carDetail.model}
+                onChange={handleFormData}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Col>
+            <Form.Group controlId="mileage">
+              <Form.Label>Mileage</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter mileage"
+                name="mileage"
+                value={carDetail.mileage}
+                onChange={handleFormData}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="color">
+              <Form.Label>Color</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter car color"
+                name="color"
+                value={carDetail.color}
+                onChange={handleFormData}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Col>
+            <Form.Group controlId="Condition" className="mb-3">
+              <Form.Label>Condition</Form.Label>
+              <Form.Control
+                as="select"
+                name="condition"
+                value={carDetail.condition}
+                onChange={handleFormData}
+              >
+                <option value="new">New</option>
+                <option value="used">Used</option>
+                <option value={"pre-owned"}>Certified Pre-Owned</option>
+              </Form.Control>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="number">
+              <Form.Label>Number</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Vehicle Identification Number"
+                name="number"
+                value={carDetail.number}
+                onChange={handleFormData}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Col>
+            <Form.Group controlId="EngineType">
+              <Form.Label>Engine Type</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter engine type"
+                name="engine_type"
+                value={carDetail.engine_type}
+                onChange={handleFormData}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="FuelType">
+              <Form.Label>Fuel Type</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter fuel type"
+                name="fuel_type"
+                value={carDetail.fuel_type}
+                onChange={handleFormData}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Col>
+            <Form.Group controlId="Transmission">
+              <Form.Label>Transmission</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter transmission type"
+                name="transmission"
+                value={carDetail.transmission}
+                onChange={handleFormData}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="Price">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter price"
+                name="price"
+                value={carDetail.price}
+                onChange={handleFormData}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Col>
+            <Form.Group controlId="RegistrationDate">
+              <Form.Label>Registration Date</Form.Label>
+              <Form.Control
+                type="date"
+                name="register_date"
+                value={carDetail.register_date}
+                onChange={handleFormData}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="Status">
+              <Form.Label>Status</Form.Label>
+              <Form.Control
+                as="select"
+                name="status"
+                value={carDetail.status}
+                onChange={handleFormData}
+              >
+                <option value="available">Available</option>
+                <option value="sold">Sold</option>
+              </Form.Control>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <FileUpload handelFileUpload={saveFileData} />
+        </Row>
+
+        <Button variant="primary" className="mt-1 save" type="button" onClick={handleSubmit}>
+          Submit
+        </Button>
+        <Button variant="primary" className="mt-1 ms-5 btn btn-danger clear" type="button" onClick={clearData}>
+          Clear
+        </Button>
+      </Form>
+      <ToastContainer />
+    </Container>
+  );
+}
---
+++ src/components/cars/CarDetail.js
@@ -0,0 +1,341 @@
+import { addDoc, collection, doc, getDoc, updateDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { db } from "../firebase/Firebase";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { useSearchParams } from "react-router-dom";
+
+import {
+  Badge,
+  Button,
+  Col,
+  Container,
+  Form,
+  Image,
+  Modal,
+  Row,
+  Spinner,
+} from "react-bootstrap";
+import moment from "moment";
+import { ToastContainer, toast } from "react-toastify";
+
+export default function CarDetail() {
+  const [car, setCar] = useState();
+  const [loader, setLoader] = useState(true);
+  const userInfo = useSelector((state) => state.app.user);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [isCarForRent, setIsCarForRent] = useState(false);
+  const [showModal,setShowModal] = useState(false)
+  const type = searchParams.get("type");
+  const currentDate = moment().format("YYYY-MM-DD");
+
+  const [carRentDetail,setCarRentDetail] = useState({
+    startDate:currentDate,
+    endDate:currentDate,
+    owner_id:null,
+    customer_id:userInfo.id,
+    no_of_day:null,
+    rent:null,
+    car_id:id
+  })
+
+  // const [startDate,setStartDate] = useState(currentDate);
+  // const [endDate,setEndDate] = useState(currentDate);
+
+  // Get specific car detail
+  const getCarDetail = async () => {
+    const carDoc = doc(db, "cars", id);
+    setLoader(true);
+    const response = await getDoc(carDoc);
+    const carDetail = response.data();
+   
+    const OWNER_ID = carDetail && carDetail.owner_id
+    // set car owner Id
+    setCarRentDetail((preview) => ({
+      ...preview,
+      owner_id:OWNER_ID
+    }))
+
+    setCar(carDetail);
+
+    getCarOwnerDetail(carDetail.owner_id);
+    if (carDetail.seller_id) {
+      getSellerUserDetail(carDetail.seller_id);
+    }
+  };
+
+  // get Car owner detail
+  const getCarOwnerDetail = async (ownerId) => {
+    const userDoc = doc(db, "users", ownerId);
+    const response = await getDoc(userDoc);
+    const owner = response.data();
+    setCar((preview) => ({
+      ...preview,
+      ownerDetail: owner,
+    }));
+    setLoader(false);
+  };
+
+  // Buy car funcation
+  function buyCar() {
+    const userRef = doc(db, "cars", id);
+    const date = moment().format("YYYY/MM/DD");
+    console.log("date: ", date);
+
+    const carDetail = {
+      status: "sold",
+      seller_id: userInfo.id,
+      sellDate: date,
+    };
+
+    updateDoc(userRef, carDetail)
+      .then(() => {
+        toast.success("Data updated");
+        getCarDetail();
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  }
+
+  // seller detail
+  const getSellerUserDetail = async (id) => {
+    setLoader(true);
+    const userDoc = doc(db, "users", id);
+    const response = await getDoc(userDoc);
+    const seller = response.data();
+    setCar((preview) => ({
+      ...preview,
+      sellerDetail: seller,
+    }));
+    setLoader(false);
+
+    setLoader(false);
+  };
+
+
+  // set car for rent form detail
+  const handelFormData = async (e) => {
+    const { name, value } = e.target;
+    setCarRentDetail((preview) => ({
+      ...preview,
+      [name]: value,
+    }));
+  };
+
+
+  //store data into firebase database
+  const getCarForRent = async () => {
+      const carRentDoc = collection(db,'rent_car_details');
+      await addDoc(carRentDoc,carRentDetail).then((response) => {
+        console.log('response: ', response);
+        toast.success('Your car is booked');
+        setShowModal(false);
+      }).catch((err) => {
+        console.log('err: ', err);
+        
+      });
+  }
+
+  useEffect(() => {
+    getCarDetail();
+    if (searchParams && type === "car_for_rent") {
+      setIsCarForRent(true);
+    }
+  }, []);
+
+  // Set No of day & rent
+  useEffect(() => {
+    let startDate = moment(carRentDetail.startDate);
+    let endDate = moment(carRentDetail.endDate);
+    
+    // Check if endDate is less than startDate
+    if(endDate.isBefore(startDate)) {
+      toast.warning('Please Select valid end date!')
+    }
+    else {
+      let dayDifference = endDate.diff(startDate, 'days') + 1;
+      const PER_DAY_RENT = 500;
+      const totalRent = PER_DAY_RENT * dayDifference;
+      setCarRentDetail((preview) => ({
+        ...preview,
+        no_of_day:dayDifference,
+        rent:totalRent
+      }))
+      console.log("Number of days:", totalRent);
+    }
+  },[carRentDetail.startDate,carRentDetail.endDate])
+
+  return (
+    <div>
+      <Button className="ms-5" onClick={() => navigate(-1)}>
+        <IoMdArrowRoundBack />
+        Back
+      </Button>
+      <Container className="mt-5">
+        {loader && (
+          <div className="text-center" style={{ marginTop: "30%" }}>
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </div>
+        )}
+        {!loader && car && (
+          <>
+            <Row>
+              <Col md={6}>
+                {car.image_url && <Image src={car.image_url} fluid />}
+              </Col>
+              <Col md={3}>
+                <h2>{car.compna_name}</h2>
+                <p>
+                  <strong>Model:</strong> {car.model}
+                </p>
+                <p>
+                  <strong>Mileage:</strong> {car.mileage}
+                </p>
+                <p>
+                  <strong>Color:</strong> {car.color}
+                </p>
+                <p>
+                  <strong>Condition:</strong> {car.condition}
+                </p>
+                <p>
+                  <strong>Status:</strong>
+                  <Badge
+                    variant="primary"
+                    className={
+                      car.status == "available" ? `bg-primary` : `bg-danger`
+                    }
+                  >
+                    {car.status}
+                  </Badge>
+                </p>
+                <p>
+                  <strong>Transmission:</strong> {car.transmission}
+                </p>
+
+                {!car.seller_id &&
+                  car.owner_id !== userInfo.id &&
+                  (isCarForRent ? (
+                    <Button onClick={() => setShowModal(true)}>Get Car For Rent</Button>
+                  ) : (
+                    <Button onClick={buyCar}> Buy</Button>
+                  ))}
+              </Col>
+              <Col md={3} className="mt-4 pt-4">
+                <p>
+                  <strong>Number:</strong> {car.number}
+                </p>
+                <p>
+                  <strong>Engine Type:</strong> {car.engine_type}
+                </p>
+                <p>
+                  <strong>Price:</strong> {car.price}
+                </p>
+                <p>
+                  <strong>Fuel Type:</strong> {car.fuel_type}
+                </p>
+                <p>
+                  <strong>Register Date:</strong> {car.register_date}
+                </p>
+                {car.sellDate && (
+                  <p>
+                    <strong>Sold Date:</strong> {car.sellDate}
+                  </p>
+                )}
+              </Col>
+            </Row>
+            {/* Car owner Details */}
+            <Row className="owner-detail">
+              <Col md={6}>
+                <Row>
+                  <Col md={6}>
+                    <span className="heading">Owner Detail</span>
+                    {car.image_url && (
+                      <Image src={car.ownerDetail.avatar} thumbnail />
+                    )}
+                  </Col>
+                  <Col md={6} className="mt-4 pt-4">
+                    <p>
+                      <strong>Name:</strong> {car.ownerDetail.full_name}
+                    </p>
+                    <p>
+                      <strong>Email:</strong> {car.ownerDetail.email}
+                    </p>
+                    <p>
+                      <strong>City:</strong> {car.ownerDetail.city}
+                    </p>
+                  </Col>
+                </Row>
+              </Col>
+              {car.sellerDetail && (
+                <Col md={6}>
+                  <Row>
+                    <Col md={6}>
+                      <span className="heading">Seller Detail</span>
+                      {car.image_url && (
+                        <Image src={car.sellerDetail.avatar} thumbnail />
+                      )}
+                    </Col>
+                    <Col md={6} className="mt-4 pt-4">
+                      <p>
+                        <strong>Name:</strong> {car.sellerDetail.full_name}
+                      </p>
+                      <p>
+                        <strong>Email:</strong> {car.sellerDetail.email}
+                      </p>
+                      <p>
+                        <strong>City:</strong> {car.sellerDetail.city}
+                      </p>
+                    </Col>
+                  </Row>
+                </Col>
+              )}
+            </Row>
+          </>
+        )}
+      </Container>
+      <ToastContainer />
+      <Modal centered show={showModal} onHide={() => setShowModal(!showModal)} size="sm">
+        <Modal.Header closeButton>
+          <Modal.Title>Car for rent </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form.Group controlId="color">
+              <Form.Label>Star date</Form.Label>
+              <Form.Control
+                type="date"
+                placeholder="Enter car color"
+                name="startDate"
+                value={carRentDetail.startDate}
+                onChange={handelFormData}
+              />
+            </Form.Group>
+            <Form.Group controlId="color">
+              <Form.Label>End date</Form.Label>
+              <Form.Control
+                type="date"
+                placeholder="Enter car color"
+                name="endDate"
+                value={carRentDetail.endDate}
+                onChange={handelFormData}
+              />
+            </Form.Group>
+            <Form.Label className="mt-2">No of Days:: {carRentDetail.no_of_day}</Form.Label>
+        </Modal.Body>
+
+        <Modal.Footer>
+        <Form.Label className="mt-2">Total::{carRentDetail.rent}</Form.Label>
+          <Button variant="primary" onClick={getCarForRent}>
+            Pay
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      
+    </div>
+  );
+}
